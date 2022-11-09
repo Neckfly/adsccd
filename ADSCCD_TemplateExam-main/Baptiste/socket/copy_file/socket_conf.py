080206631fb8c0a1eaa872f72e61d5af49ce6719
Neckfly <45126049+Neckfly@users.noreply.github.com>
2022-11-09 13:40:38 +0100

    Delete ADSCCD_TemplateExam-main directory

--- ADSCCD_TemplateExam-main/Baptiste/socket/copy_file/socket_conf.py
+++
@@ -1,2 +0,0 @@
-server_port = 60000
-server_port_2 = 60001
--- ADSCCD_TemplateExam-main/socket_client/socketClient.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-from socket_tcp_settings import server_address, server_port, struc_nb_bytes
-
-#Creation de la socket client
-connected_socket = socket.create_connection((server_address, server_port))
-print(f"Client paired succesfully")
